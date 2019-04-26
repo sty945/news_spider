5bc2e432dead4d0ddd0b07a9ccd350aec1d1fa9a
sty945 <sutaoyu@yeah.net>
2019-04-26 10:30:07 +0800

    从数据库中读取数据写入文件

--- bin/writefile.py
+++ bin/writefile.py
@@ -1,36 +1,27 @@
-from homework1 import item_info
+from news_spider import item_info
 import codecs
 import os
-import sys
 
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '\\contents\\'
-
-def write_single_file(file):
+def write_context(file_name, month_name='01'):
+    file = codecs.open(file_name, 'w+', 'utf-8')
     num = 0
     i = 0
-    file = codecs.open(file, 'w+', 'utf-8')
     for data in item_info.find():
-        try :
+        try:
             i += 1
-            #link = data["link"]
-            month = data["month"]
-            if '11' in month:
-                content = data['content']
+            month = data["date"]["month"]
+            if month_name in month:
+                content = data["content"]
                 file.write(content)
         except:
             num += 1
             print("error:" + str(num) + " num:" + str(i) + " " + str(data["_id"]))
     file.close()
 
-def write_all_file(start_num, end_num):
-    for i in range(start_num, end_num):
-        if i < 10:
-            file_name = '0' + str(i)
-        else:
-            file_name = str(i)
-        file_name += 'content'
-        file = path + file_name + '.txt'
-        write_single_file(file)
-print(path)
-write_all_file(11, 12)
-
+if __name__ == '__main__':
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '\\contents\\'
+    print(path)
+    month_name = '03'
+    file_name = path + month_name + '_content.txt'
+    print(file_name)
+    write_context(file_name, month_name)
